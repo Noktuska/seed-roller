3b18396371b8730933473df9e7bb9c5518af2dd5
Loptr <tjahn0612@gmail.com>
2026-07-22 16:19:13 +0200

    Update diff.lua to not count same strats multiple times

--- lua_examples/diff.lua
+++ lua_examples/diff.lua
@@ -1,13 +1,19 @@
 function eval(new_spoiler, old_spoiler)
-	if old_spoiler == nil then return true end
-	
 	get_diff = function(spoiler)
 		diff = 0
+		seen_tech = {}
 		for _, details in ipairs(spoiler.details) do
 			for _, item in ipairs(details.items) do
 				if item.difficulty ~= nil then
 					for _, route_entry in ipairs(item.obtain_route) do
-						diff = diff + 2 ^ route_entry.strat_difficulty
+						if route_entry.strat_id ~= nil then
+							if not seen_tech[route_entry.strat_id] then
+								diff = diff + 2 ^ route_entry.strat_difficulty
+							end
+							seen_tech[route_entry.strat_id] = true
+						else
+							diff = diff + 2 ^ route_entry.strat_difficulty
+						end
 					end
 					break
 				end
@@ -16,7 +22,10 @@ function eval(new_spoiler, old_spoiler)
 		return diff
 	end
 	
-	diff_old = get_diff(old_spoiler)
+	diff_old = 0
+	if old_spoiler ~= nil then
+		diff_old = get_diff(old_spoiler)
+	end
 	diff_new = get_diff(new_spoiler)
 
 	print("diff_new: ", diff_new)
